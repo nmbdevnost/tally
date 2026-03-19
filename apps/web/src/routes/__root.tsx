a4f35ec0b8d54f4d5ccafda035c917713a56283a
dev user8 <adarsh.kunwar@amniltech.com>
2026-03-19 13:38:30 +0545

    chore: change naming from workspace to tally

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -1,6 +1,6 @@
 import { HeadContent, Scripts, createRootRoute } from "@tanstack/react-router"
 
-import appCss from "@workspace/ui/globals.css?url"
+import appCss from "@tally/ui/globals.css?url"
 
 export const Route = createRootRoute({
   head: () => ({
